0726f2f6c1fde2692bb3e05517b08b0a8a797b4e
Paweł Wojtuń <panpawelw@pm.me>
2022-02-12 23:59:08 +0100

    Initial commit

---
+++ src/main/java/com/panpawelw/clientdetailswebapp/ClientdetailswebappApplication.java
@@ -0,0 +1,13 @@
+package com.panpawelw.clientdetailswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientdetailswebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientdetailswebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/panpawelw/clientdetailswebapp/ClientdetailswebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panpawelw.clientdetailswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientdetailswebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
