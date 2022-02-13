adac0e1ff5cbcb84abb8503888a89234f2dac297
Paweł Wojtuń <panpawelw@pm.me>
2022-02-13 16:06:10 +0100

    Multiple architectural and configuration changes:
    -change packaging to .jar
    -change application port to 8081
    -add banner to indicate the application has started
    -add README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+All that's necessary to run this application is to clone it from the repo and run:
+
+    mvn spring-boot:run
+
+from the command line in the main application directory, where pom.xml file is located.
+
+When the maven and spring boot output is finished a small banner will appear in the command line:
+
+```
+*****************************************************************
+* Clientdetails Web Application has launched. Please head on to *
+*  http://localhost:8081/clientdetailswebapp/ in your browser!  *
+*****************************************************************
+```
+
+Please follow this instruction.
--- src/main/java/com/panpawelw/clientdetailswebapp/ClientdetailswebappApplication.java
+++ src/main/java/com/panpawelw/clientdetailswebapp/ClientdetailswebappApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClientdetailswebappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientdetailswebappApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ClientdetailswebappApplication.class, args);
+    System.out.println("*****************************************************************");
+    System.out.println("* Clientdetails Web Application has launched. Please head on to *");
+    System.out.println("*  http://localhost:8081/clientdetailswebapp/ in your browser!  *");
+    System.out.println("*****************************************************************");
+  }
 
 }
